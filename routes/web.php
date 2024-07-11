569b1c2aa18992e42176ec2d572eb8dc4de0420a
OkuhleBastion <okuhlen@bastionflowe.com>
2024-07-11 13:17:54 +0200

    Dashboard route uncommented

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::redirect('/', 'login');
 
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
 
-    //Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/dashboard/enquiries', CustomerEnquiries::class)->name('enquiries');
     Route::get('/dashboard/customers', Customers::class)->name('customers');
     Route::get('/dashboard/transactions', Transactions::class)->name('transactions');
